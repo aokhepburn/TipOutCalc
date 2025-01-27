8a62c93de3133ad0a22189525887fddc721085df
Alice Hepburn <aliceokhepburn@gmail.com>
2025-01-27 10:42:54 -0500

    ADDS beginning calculation functions to basic no oop concept

--- python_v0/no_oop1.py
+++ python_v0/no_oop1.py
@@ -13,44 +13,76 @@ program collects
 total_tips
 total_hours"""
 
-employees = {}
+# filled in for testing only
+employees = {
+    "Alice": {
+        "role": "bartender",
+        "tips earned": 120,
+        "hourly wage": 10,
+        "hours worked": 5,
+    },
+    "James": {
+        "role": "server",
+        "tips earned": 80,
+        "hourly wage": 5,
+        "hours worked": 10,
+    },
+}
 total_hours = 0
 total_tips = 0
 
-# name=input("Employee:Name")
-# role=input("Employee:Role")
-# tips_earned=input("Employee:Tips Earned")
-# hourly_wage=input("Employee:Hourly Wage")
-# hours_worked=input("Employee:Hours Worked")
+
+# def add_new_employee():
+#     name = input("Employee:Name")
+#     role = input("Employee:Role")
+#     tips_earned = int(input("Employee:Tips Earned"))
+#     hourly_wage = int(input("Employee:Hourly Wage"))
+#     hours_worked = int(input("Employee:Hours Worked"))
+
+#     """When we expand programme role will play a bigger role indicating tip out or pool. """
+#     employees.update(
+#         {
+#             name: {
+#                 "employee role": role,
+#                 "tips earned": tips_earned,
+#                 "hourly wage": hourly_wage,
+#                 "hours worked": hours_worked,
+#             }
+#         }
+#     )
+
+#     add_more_employees = input("Add another employee Y/N").lower()
+#     if add_more_employees == "y":
+#         add_new_employee()
+#     else:
+#         '''next step print() will just become return and then '''
+#         print(employees)
 
 
-def add_new_employee():
-    name = input("Employee:Name")
-    role = input("Employee:Role")
-    tips_earned = input("Employee:Tips Earned")
-    hourly_wage = input("Employee:Hourly Wage")
-    hours_worked = input("Employee:Hours Worked")
-    """When we expand programme role will play a bigger role indicating tip out or pool. """
-    employees.update(
-        {
-            name: {
-                "employee role": role,
-                "tips earned": tips_earned,
-                "hourly wage": hourly_wage,
-                "hours worked": hours_worked,
-            }
-        }
-    )
+def collect_shift_information():
+    global total_tips
+    global total_hours
+    for employee in employees:
+        total_tips += int(employees[employee]["tips earned"])
+        total_hours += int(employees[employee]["hours worked"])
 
-    add_more_employees = input("Add another employee Y/N").lower()
-    if add_more_employees == "y":
-        add_new_employee()
-    else:
-        '''next step print() will just become return and then '''
-        print(employees)
+    print(total_tips)
+    print(total_hours)
 
 
-add_new_employee()
+def calculate_tip_hourly():
+    global total_tips
+    global total_hours
+    hourly = total_tips / total_hours
+    for employee in employees:
+        hourly = hourly * employees[employee]["hours worked"]
+        employees[employee].update({"walk tips": hourly})
+        print(employee, employees[employee]["walk tips"])
+
+
+# add_new_employee()
+collect_shift_information()
+calculate_tip_hourly()
 exit()
 """extend programme to ask if there is another worker"""
 
